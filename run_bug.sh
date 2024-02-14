56bb06935a6b6c2cce0d7f16390c59f28449b1ee
velle <mrvelle@gmail.com>
2024-02-14 08:14:47 +0100

    run_bug.sh

--- run_bug.sh
+++ run_bug.sh
@@ -1,1 +1,15 @@
-bundle exec ruby -w -I"lib:lib:test" /home/velle/a/jekyll/vendor/bundle/ruby/3.0.0/gems/rake-13.1.0/lib/rake/rake_test_loader.rb "test/test_cleaner_foo.rb" "test/test_filters_foo.rb"
+#bundle exec ruby -w -I"lib:lib:test" /home/velle/a/jekyll/vendor/bundle/ruby/3.0.0/gems/rake-13.1.0/lib/rake/rake_test_loader.rb "test/test_cleaner_foo.rb" "test/test_filters_foo.rb"
+
+bundle exec ruby -w -I"lib:lib:test" \
+  /home/velle/a/jekyll/vendor/bundle/ruby/3.0.0/gems/rake-13.1.0/lib/rake/rake_test_loader.rb \
+  "test/test_cleaner_foo.rb" "test/test_filters_foo.rb"
+
+#  --seed=40420 \
+
+
+# TESTOPTS="--seed=1261"
+
+#bundle exec rake test TEST=test/test_filters_foo.rb
+
+
+bundle exec rake test
