48c694458dbc74f8f974d426cc55b4d839f18b2a
velle <mrvelle@gmail.com>
2024-02-14 06:54:45 +0100

    run_bug.sh test_filters_foo.rb

---
+++ run_bug.sh
@@ -0,0 +1,1 @@
+bundle exec ruby -w -I"lib:lib:test" /home/velle/a/jekyll/vendor/bundle/ruby/3.0.0/gems/rake-13.1.0/lib/rake/rake_test_loader.rb "test/test_cleaner_foo.rb" "test/test_filters_foo.rb"
